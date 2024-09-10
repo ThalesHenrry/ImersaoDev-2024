11308bc161426fec760e9f20e8499dc8708b4df1
Thales Gregório <104868991+ThalesHenrry@users.noreply.github.com>
2024-09-09 21:27:58 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,47 @@
+function pesquisar() {
+    console.log(anime);
+
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    if(!campoPesquisa){
+        section.innerHTML = "<p>Nenhum anime encontrado!</p>"
+        return
+    }
+    
+    let resultado = "";
+    let titulo = "";
+    let sinopse = "";
+    let tags = "";
+    
+    for (let item of anime) {
+        titulo = item.titulo.toLowerCase()
+        sinopse = item.sinopse.toLowerCase()
+        tags = item.tags.toLowerCase()
+
+        if (titulo.includes(campoPesquisa) || sinopse.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            resultado += `
+        <div class="item-resultado">
+            <img class="imagem-anime" src="${item.imagem}" alt="${item.titulo}">
+            <div class="texto-anime">
+                <h2>${item.titulo}</h2>
+                <p class="descricao-meta">${item.sinopse}</p>
+                <a href="https://pt.wikipedia.org/wiki/${item.titulo.replace(/\s+/g, '_')}" target="_blank">Mais informações</a>
+            </div>
+        </div>
+        `;
+        }
+
+        if (!resultado){
+            resultado = "<p>Anime não enontrado</p>"
+        }
+    }
+    
+    section.innerHTML = resultado
+
+}
+
+
+
+
---
+++ dados.js
@@ -0,0 +1,23 @@
+let anime = [
+{
+        imagem: "img/frieren.jpg",
+        titulo: "Frieren",
+        sinopse: "Frieren é um anime que explora a jornada de um jovem em um mundo pós-apocalíptico, onde a humanidade luta para sobreviver em um ambiente hostil. Com elementos de ação, aventura e drama, a série acompanha o protagonista em sua busca por respostas sobre o passado e seu papel no futuro da humanidade.",
+        link: "https://pt.wikipedia.org/wiki/wiki/Frieren",
+        tags: "elfa aventura imortal magia"
+    },
+    {
+        imagem: "img/kimetsu.jpg",
+        titulo: "Kimetsu no Yaiba",
+        sinopse: "Kimetsu no Yaiba é um anime de ação e fantasia que se passa no Japão Taisho. A história acompanha Tanjiro Kamado, um jovem que se torna um caçador de demônios após sua família ser massacrada e sua irmã mais nova transformada em um demônio. Em busca de uma cura para sua irmã, Tanjiro enfrenta diversos desafios e inimigos sobrenaturais.",
+        link: "https://pt.wikipedia.org/wiki/Demon_Slayer_(mangá)",
+        tags: "demonios onis espadachim respiração"
+    },
+    {
+        imagem: "img/jujutsu.jpg",
+        titulo: "Jujutsu Kaisen",
+        sinopse: "Jujustsu Kaisen é um anime de ação e sobrenatural que gira em torno de Yuji Itadori, um estudante do ensino médio que, ao ingerir um dedo amaldiçoado, se torna um recipiente para um poderoso espírito amaldiçoado. Agora, ele é forçado a entrar no mundo da magia jujutsu para proteger a humanidade e encontrar uma maneira de quebrar a maldição.",
+        link: "https://pt.wikipedia.org/wiki/Jujutsu_Kaisen",
+        tags: "demonios sukuna magia magos dominio"
+    }
+];
